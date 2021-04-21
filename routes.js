bfb0e0093a351324faf56adf9849a546bad28b91
Your Name <you@example.com>
2021-04-21 18:38:49 -0500

    EndPoint de getpedidos por tienda

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
-
+const axios = require('axios');
+const fetch = require('node-fetch');
 routes.get('/getRubros',(req,res)=>{
     req.getConnection((err,conn) => {
         if (err) {
@@ -118,7 +119,7 @@ routes.post('/registerItem',(req,res)=>{
         if (err) {
             return res.send(err)
         }else{
-            conn.query('INSERT INTO `TM_ITEM`(`NO_ITEM`, `ITEM_DESCRIPCION`, `UN_MEDI`, `CA_STOC`, `PREC`, `LI_IMAG`, `ES_ITEM`, `FE_CREA_USUA`, `CO_MODI_USUA`, `CO_CATE`, `CO_TIEN`) VALUES (?,?,?,?,?,?,?,?,?,?,?)' , [req.body.item_nombre,req.body.item_descripcion,req.body.item_medida,req.body.item_stock,req.body.item_precio,req.body.item_link_imagen,req.body.item_estado,req.body.item_fecha,req.body.item_modi_usua,req.body.item_codigo_categoria,req.body.item_codigo_tienda] , (err,rows) => {
+            conn.query('INSERT INTO `TM_ITEM`(`NO_ITEM`, `ITEM_DESCRIPCION`, `UN_MEDI`, `CA_STOC`, `PREC`, `LI_IMAG`,`TIPO_ITEM`, `ES_ITEM`, `FE_CREA_USUA`, `CO_MODI_USUA`, `CO_CATE`, `CO_TIEN`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)' , [req.body.item_nombre,req.body.item_descripcion,req.body.item_medida,req.body.item_stock,req.body.item_precio,req.body.item_link_imagen,req.body.tipo_item,req.body.item_estado,req.body.item_fecha,req.body.item_modi_usua,req.body.item_codigo_categoria,req.body.item_codigo_tienda] , (err,rows) => {
                 if (err) {
                     return res.send(err)
                 }else{
@@ -253,6 +254,22 @@ routes.get('/getDataUser/:id_usuario',(req,res)=>{
     })
 })
 
+routes.get('/getPedidosxTienda/:id_tienda',(req,res)=>{
+    req.getConnection((err,conn) => {
+        if (err) {
+            return res.send(err)
+        }else{
+            conn.query('SELECT * FROM `TM_PEDIDOS` WHERE ID_TIENDA = ?' , [req.params.id_tienda] , (err,rows) => {
+                if (err) {
+                    return res.send(err)
+                }else{
+                    res.json(rows)
+                }
+            })
+        }   
+    })
+})
+
 routes.post('/LoginTienda',(req,res)=>{
     req.getConnection((err,conn) => {
         if (err) {
@@ -284,5 +301,71 @@ routes.post('/LoginRepartidor',(req,res)=>{
         }
     })
 })//LISTO
+
+routes.post('/SaveTokenRepartidor',(req,res)=>{
+    req.getConnection((err,conn) => {
+        if (err) {
+            return res.send(err)
+        }else{
+            conn.query('UPDATE TM_USUA_REPARTIDOR SET RE_TOKEN_FCM = ? WHERE TM_USUA_REPARTIDOR.CO_REPARTIDOR = ?' , [req.body.token_fcm,req.body.co_repatidor] , (err,rows) => {
+                if (err) {
+                    return res.send(err)
+                }else{
+                    res.json(rows)
+                }
+            })
+        }
+    })
+})//LISTO
+routes.post('/SendNotificacion',(req,res)=>{
+    req.getConnection((err,conn) => {
+        if (err) {
+            return res.send(err)
+        }else{
+            var notification = {
+                'title' : 'Tienes un nuevo pedido',
+                'text' : 'Llevelo Repartidor'
+            };
+            var fcm_tokens=['dGPxBm-VfEc:APA91bESI8AQ6vLdfstPqBjIXeItIPPhBLM1ZuMB9bwCUKzFmHgeCKl4xoA-NFpwnLqVJqILZ5WtDUV93l27aD1HIc-Ukr21rwZB9XJ-PaPsEX6NFGdDKWTk-cfpTZUN8XCJ0pN4sbzp'];
+
+            var notification_body = {
+                'notification':notification,
+                'registration_ids':fcm_tokens
+            }
+
+            fetch('https://fcm.googleapis.com/fcm/send',{
+                'method':'POST',
+                'headers':{
+                    'Authorization':'key='+'AAAAshVWmgw:APA91bFV_KddpUvhHWZdWoHqXufQRuDwl_21X28LkjA5ySPz9GrnOk4JDOKOpO2OaIJ-_kAdRI_yQ17fCppZSnQ9ha75maCgIo-x9bPy8M3aoAu6ogUy4CBn7HmvrXgEjf34pzHlngZa',
+                    'Content-Type':'application/json'
+                },
+                'body':JSON.stringify(notification_body)
+            }).then(()=>{
+                res.status(200).send('Notificacion enviada con exito')
+            }).catch((err)=>{
+                res.status(200).send('Error en enviar la notificacion')
+                console.log(err);
+            })
+        }
+    })
+})
+// {
+//     "notification":{
+//       "title":"Ionic 4 Notification",
+//       "body":"This notification sent from POSTMAN using Firebase HTTP protocol",
+//       "sound":"default",
+//       "click_action":"FCM_PLUGIN_ACTIVITY",
+//       "icon":"fcm_push_icon"
+//     },
+//     "data":{
+//       "landing_page":"second",
+//       "price":"$3,000.00"
+//     },
+//       "to":"eadego-nig0:APA91bEtKx9hv50lmQmfzl-bSDdsZyTQ4RkelInfzxrPcZjJaSgDmok3-WQKV5FBu9hrMrkRrcCmf3arkGSviGltg5CyC2F9x1J2m0W7U8PxJ3Zlh7-_tL6VcFdb76hbaLIdZ-dOK15r",
+//       "priority":"high",
+//       "restricted_package_name":""
+//   }
+
+
 
 module.exports = routes
